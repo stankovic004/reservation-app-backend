7f3d40d3ab6846c7194f1f5e09be0623923a95f1
stankovi004 <aleksandar.stankovic004@gmail.com>
2022-04-28 21:32:41 +0200

    fixed something

--- interfaces/interfaces.go
+++ interfaces/interfaces.go
@@ -1,7 +1,22 @@
 package interfaces
 
+import (
+	"time"
+)
+
+type resComponent struct {
+	hour     int
+	min      int
+	occupied bool
+}
+
 type User struct {
 	Email    string `json:"email"`
 	Username string `json:"username"`
 	Password string `json:"password"`
+}
+
+type Reservation struct {
+	Date     time.Time    `json:"date"`
+	Schedule resComponent `json:"schedule"`
 }
